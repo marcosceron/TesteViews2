afb95d22c65fade2f97da52b0188ed10be99c305
Marcos <marcos.ceron89@gmail.com>
2025-06-25 14:39:55 -0300

    Exercícios 1 e 2

--- app/src/main/java/com/example/testeviews2/MainActivity.kt
+++ app/src/main/java/com/example/testeviews2/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.testeviews2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val botao: Button = findViewById(R.id.botao)
+        botao.setOnClickListener {
+            Toast.makeText(this, "Botão clicado!", Toast.LENGTH_SHORT).show()
         }
     }
 }
