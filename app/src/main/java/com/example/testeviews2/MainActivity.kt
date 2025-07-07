da16b308bdcdfc69e40c9c48d35353f99aa27d9b
Marcos <marcos.ceron89@gmail.com>
2025-07-07 14:34:02 -0300

    Exercícios 3 a 5

--- app/src/main/java/com/example/testeviews2/MainActivity.kt
+++ app/src/main/java/com/example/testeviews2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.testeviews2
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,13 @@ class MainActivity : AppCompatActivity() {
         val botao: Button = findViewById(R.id.botao)
         botao.setOnClickListener {
             Toast.makeText(this, "Botão clicado!", Toast.LENGTH_SHORT).show()
+        }
+
+        val inputText: EditText = findViewById<EditText>(R.id.input_text)
+        val botaoMostrar: Button = findViewById<Button>(R.id.mostrar_texto)
+        botaoMostrar.setOnClickListener {
+            val text = inputText.text.toString()
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
         }
     }
 }
